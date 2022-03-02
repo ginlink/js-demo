bc6616902aea1934acc58ed71f4632ba3f259106
ginlink <ginlink@163.com>
2022-03-02 22:12:22 +0800

    feat: add 030.152.乘积最大子数组

--- src/02_Leetcode/025.动态求解凑出硬币方式.ts
+++ src/02_Leetcode/025.动态求解凑出硬币方式.ts
@@ -1,8 +1,9 @@
 function main() {
   const coinFace = [1, 5, 11]
-  const w = 15
+  const w = 16
 
   console.log('[]:', coinWaysNum(coinFace, w))
+  console.log('[]:', coinWaysNum111(coinFace, w))
 }
 
 main()
@@ -22,4 +23,23 @@ function coinWaysNum(coinFace: number[], w: number): number {
   }
 
   return fn[w]
+}
+function coinWaysNum111(coinFace: number[], w: number): number {
+  if (w < 1) return undefined
+
+  let minF = new Array(w).fill(0)
+
+  for (let i = 1; i <= w; ++i) {
+    let cost = Infinity
+
+    if (i - 1 >= 0) cost = Math.min(cost, minF[i - 1] + 1)
+    if (i - 5 >= 0) cost = Math.min(cost, minF[i - 5] + 1)
+    if (i - 11 >= 0) cost = Math.min(cost, minF[i - 11] + 1)
+
+    minF[i] = cost
+  }
+
+  console.log('[]:', minF)
+
+  return minF[w]
 }
---
+++ src/02_Leetcode/030.152.乘积最大子数组.ts
@@ -0,0 +1,50 @@
+function main() {
+  // const nums = [2, 3, -2, 4]
+  // const nums = [-2, 0, -1]
+  // const nums = []
+  // const nums = [-2]
+  const nums = [-3, 0, 1, -2]
+
+  console.log('[]:', maxProduct(nums))
+  console.log('[]:', maxProduct111(nums))
+}
+
+main()
+
+function maxProduct(nums: number[]): number {
+  let res = -Infinity
+
+  for (let i = 0; i < nums.length; ++i) {
+    let mul = nums[i]
+    res = Math.max(res, mul)
+
+    for (let j = i + 1; j < nums.length; ++j) {
+      mul *= nums[j]
+
+      res = Math.max(res, mul)
+    }
+  }
+
+  return res == -Infinity ? undefined : res
+}
+
+function maxProduct111(nums: number[]): number {
+  const len = nums.length
+  let maxF = new Array(len).fill(0)
+  let minF = new Array(len).fill(0)
+
+  maxF[0] = nums[0]
+  minF[0] = nums[0]
+
+  for (let i = 1; i < len; ++i) {
+    maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(minF[i - 1] * nums[i], nums[i]))
+    minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(maxF[i - 1] * nums[i], nums[i]))
+  }
+
+  let res = maxF[0]
+  for (let i = 1; i < len; ++i) {
+    res = Math.max(res, maxF[i])
+  }
+
+  return res
+}
