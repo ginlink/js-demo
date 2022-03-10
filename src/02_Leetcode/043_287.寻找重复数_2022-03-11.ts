b1a0820031ccb91ee1202bb4c632e756e5884ab7
ginlink <ginlink@163.com>
2022-03-11 06:19:36 +0800

    ref: ref review of 287

--- src/02_Leetcode/043_287.寻找重复数_2022-03-11.ts
+++ src/02_Leetcode/043_287.寻找重复数_2022-03-11.ts
@@ -1,3 +1,4 @@
+// binary search
 function findDuplicate(nums: number[]): number {
   const len = nums.length
   let l = 0,
@@ -5,7 +6,9 @@ function findDuplicate(nums: number[]): number {
     ans = -1
 
   while (l <= r) {
-    const mid = Math.floor((r + l) / 2)
+    const mid = Math.floor((l + r) / 2)
+
+    console.log('[]:', l, mid, r)
 
     let cnt = 0
     for (let i = 0; i < len; ++i) {
@@ -14,7 +17,6 @@ function findDuplicate(nums: number[]): number {
       }
     }
 
-    console.log('[]:', r, mid, l, cnt)
     if (cnt <= mid) {
       l = mid + 1
     } else {
@@ -26,6 +28,7 @@ function findDuplicate(nums: number[]): number {
   return ans
 }
 
+// fast and slow pointer
 function findDuplicate111(nums: number[]): number {
   let slow = 0,
     fast = 0
@@ -34,26 +37,33 @@ function findDuplicate111(nums: number[]): number {
     slow = nums[slow]
     fast = nums[nums[fast]]
 
-    if (fast == slow) {
+    if (slow === fast) {
       break
     }
   }
 
   let find = 0
   while (true) {
-    find = nums[find]
     slow = nums[slow]
-    if (find == slow) {
+    find = nums[find]
+
+    if (slow === find) {
       break
     }
   }
 
   return find
+
+  // error
+  // const found = nums.find((x) => x === nums[slow])
+  // return found === undefined ? -1 : nums[found]
 }
 
 function main() {
-  const nums = [1, 3, 4, 2, 2]
+  // const nums = [1, 3, 4, 2, 2]
   // const nums = [1, 2, 4, 2, 3]
+  // const nums = [1, 1]
+  const nums = [2, 5, 9, 6, 9, 3, 8, 9, 7, 1]
 
   console.log('[]:', findDuplicate(nums))
   console.log('[]:', findDuplicate111(nums))
