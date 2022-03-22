4837a14ed1390dd7fbc02c1ae2ac97b2bc35d194
ginlink <ginlink@163.com>
2022-03-22 17:11:13 +0800

    feat: add 739

--- src/02_Leetcode/060_739. 每日温度.ts
+++ src/02_Leetcode/060_739. 每日温度.ts
@@ -7,7 +7,6 @@ function dailyTemperatures(temperatures: number[]): number[] {
       if (temperatures[j] > temperatures[i]) {
         answer[i] = j - i
 
-        console.log('[i, j]:', i, j)
         break
       }
     }
@@ -20,11 +19,43 @@ function dailyTemperatures(temperatures: number[]): number[] {
   return answer
 }
 
+function dailyTemperatures111(temperatures: number[]): number[] {
+  const len = temperatures.length
+
+  if (len <= 0) {
+    return []
+  }
+
+  // store pos
+  const stack: number[] = []
+  const answer: number[] = new Array(len).fill(0)
+
+  stack.push(0)
+  for (let i = 1; i < len; ++i) {
+    while (temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const pos = stack.pop()
+
+      answer[pos] = i - pos
+    }
+
+    stack.push(i)
+  }
+
+  // while (stack.length > 0) {
+  //   const pos = stack.pop()
+
+  //   answer[pos] = 0
+  // }
+
+  return answer
+}
+
 function main() {
-  // const temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-  const temperatures = [30, 40, 50, 60]
+  const temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+  // const temperatures = [30, 40, 50, 60]
 
   console.log('[]:', dailyTemperatures(temperatures))
+  console.log('[]:', dailyTemperatures111(temperatures))
 }
 
 main()
